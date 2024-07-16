8ac5644597df4b411a7575b23e956205d47ed71c
Gaetan Azema <gaetan.azema@gmail.com>
2024-07-16 14:57:41 +0000

    Test de la fonction de récup de favicon fournie par chatGPT

--- public/favicon.php
+++ public/favicon.php
@@ -1,1 +1,97 @@
-<?phprequire_once($_SERVER['DOCUMENT_ROOT']."/classes/logs.php");$expires = 60*60*24;header("Pragma: public");header("Cache-Control: maxage=".$expires);header('Expires: ' . gmdate('D, d M Y H:i:s', time()+$expires) . ' GMT');function getFavicon($url) {	if(substr(strtolower($url),0,4) != "http")		return false;	$handle = @fopen($url, "rb");	if($handle){		$html = stream_get_contents($handle);		fclose($handle);		if (preg_match('/<link[^>]+rel="(?:shortcut )?icon"[^>]+?href="([^"]+?)"/si', $html, $matches)) {			$linkUrl = html_entity_decode($matches[1]); 			if (substr($linkUrl, 0, 1) == '/') {				$urlParts = parse_url($url); 				$faviconURL = $urlParts['scheme'].'://'.$urlParts['host'].$linkUrl; 			} elseif (substr($linkUrl, 0, 7) == 'http://') { 				$faviconURL = $linkUrl; 			} elseif (substr($url, -1, 1) == '/') { 				$faviconURL = $url.$linkUrl; 			} else { 				$faviconURL = $url.'/'.$linkUrl; 			} 		}else{			$urlParts = parse_url($url); 			$faviconURL = $urlParts['scheme'].'://'.$urlParts['host'].'/favicon.ico';		}		$HTTPRequest = @fopen($faviconURL, 'r'); 		if ($HTTPRequest) {			return $faviconURL;		}	}	return false;}$icon = getFavicon($_GET["url"]);if($icon == false){	header("Content-Type: image/png");	readfile("design/images/favicon.png");}else{	header("Content-Type: image/x-icon");	readfile($icon);}?>
+<?php
+require_once($_SERVER['DOCUMENT_ROOT']."/classes/logs.php");
+
+$expires = 60*60*24;
+header("Pragma: public");
+header("Cache-Control: maxage=".$expires);
+header('Expires: ' . gmdate('D, d M Y H:i:s', time()+$expires) . ' GMT');
+
+/*
+function getFavicon($url) {
+	if(substr(strtolower($url),0,4) != "http")
+		return false;
+	$handle = @fopen($url, "rb");
+	if($handle){
+		$html = stream_get_contents($handle);
+		fclose($handle);
+		if (preg_match('/<link[^>]+rel="(?:shortcut )?icon"[^>]+?href="([^"]+?)"/si', $html, $matches)) {
+			$linkUrl = html_entity_decode($matches[1]); 
+			if (substr($linkUrl, 0, 1) == '/') {
+				$urlParts = parse_url($url); 
+				$faviconURL = $urlParts['scheme'].'://'.$urlParts['host'].$linkUrl; 
+			} elseif (substr($linkUrl, 0, 7) == 'http://') { 
+				$faviconURL = $linkUrl; 
+			} elseif (substr($url, -1, 1) == '/') { 
+				$faviconURL = $url.$linkUrl; 
+			} else { 
+				$faviconURL = $url.'/'.$linkUrl; 
+			} 
+		}else{
+			$urlParts = parse_url($url); 
+			$faviconURL = $urlParts['scheme'].'://'.$urlParts['host'].'/favicon.ico';
+		}
+		$HTTPRequest = @fopen($faviconURL, 'r'); 
+		if ($HTTPRequest) {
+			return $faviconURL;
+		}
+	}
+	return false;
+}
+*/
+
+/* Test de la fonction fournie par chatGPT */
+function getFavicon($url) {
+  // Ajouter http si l'URL ne contient pas de schéma
+  if (!preg_match('~^(?:f|ht)tps?://~i', $url)) {
+      $url = 'http://' . $url;
+  }
+
+  // Utiliser cURL pour obtenir le contenu de la page
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Suivre les redirections
+  $html = curl_exec($ch);
+
+  // Vérifier les erreurs
+  if (curl_errno($ch)) {
+      echo 'Erreur cURL : ' . curl_error($ch);
+      curl_close($ch);
+      return false;
+  }
+  curl_close($ch);
+
+  // Analyser le HTML pour trouver le favicon
+  $doc = new DOMDocument();
+  @$doc->loadHTML($html);
+  $links = $doc->getElementsByTagName('link');
+
+  foreach ($links as $link) {
+      $rel = $link->getAttribute('rel');
+      if (strpos($rel, 'icon') !== false) {
+          $faviconUrl = $link->getAttribute('href');
+          
+          // Vérifier si l'URL est relative ou absolue
+          if (!preg_match('~^(?:f|ht)tps?://~i', $faviconUrl)) {
+              $faviconUrl = rtrim($url, '/') . '/' . ltrim($faviconUrl, '/');
+          }
+
+          return $faviconUrl;
+      }
+  }
+
+  // Retourner une favicon par défaut si aucune n'est trouvée
+  return rtrim($url, '/') . '/favicon.ico';
+}
+
+$icon = getFavicon($_GET["url"]);
+if($icon == false)
+{
+	header("Content-Type: image/png");
+	readfile("design/images/favicon.png");
+}
+else
+{
+	header("Content-Type: image/x-icon");
+	readfile($icon);
+}
+?>
