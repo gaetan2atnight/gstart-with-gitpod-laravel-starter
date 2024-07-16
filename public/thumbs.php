e5adac5986f365fb23f35d356c81f6db44c5e73e
Gaetan Azema <gaetan.azema@gmail.com>
2024-07-16 14:37:58 +0000

    J'enlève la fonction de suppression de favoris en mode thumb car on n'a pas la fonction de modification, c'est plus logique et cela règle les problèmes de css

--- public/thumbs.php
+++ public/thumbs.php
@@ -13,7 +13,6 @@ foreach($arrayFavoris as $label => $url){
 	echo "<img src='/return_thumb.php?site=".urlencode($url)."' border='0' class='img_thumb' onclick='window.open(\"".$url."\")'/>";
 	//echo "<img src='http://images.thumbshots.com/image.aspx?cid=GFPjl%2femoRY%3d&v=1&w=150&url=".$url."' border='0' class='img_thumb' onclick='window.open(\"".$url."\")'/>";
 	//echo "<img src='http://images.shrinktheweb.com/xino.php?stwembed=1&stwaccesskeyid=f1c956e522b51c2&stwsize=sm&stwurl=www.google.fr'/>";
-	echo "<img border='0' alt='Supprimer le favoris' src='/design/images/delete.png' onclick=\"deleteFavoris('".$label."')\"/>";
 	echo "<div class='labelThumb'>".$label."</div>";
 	echo "</div>";
 }
